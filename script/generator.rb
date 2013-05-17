fb450be54dd3bd619b664fe9d5e3198f33740eb8
Justin Jaffray <justin.jaffray@gmail.com>
2013-05-17 13:35:54 -0400

    Made some changes to scrambler generator script to allow slightly easier changing of the kind of scramble

--- script/generator.rb
+++ script/generator.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
+require "fileutils"
 
 MAX_LENGTH = 50
 COUNT = 10000
 
-SCRAMBLE_KIND = "6gen"
+SCRAMBLE_KIND = "5gen"
+
+scramble_dir = "scrambles/#{SCRAMBLE_KIND}"
+
+FileUtils.mkdir_p scramble_dir unless Dir.exist? scramble_dir
 
 0.upto(MAX_LENGTH) do |length|
-  `ramsdel -c #{COUNT} -e "<R,L,U,D,F,B> * #{length}" > scrambles/#{SCRAMBLE_KIND}/len_#{length}`
-
+  `ramsdel -c #{COUNT} -e "<R,L,U,F,B> * #{length}" > #{scramble_dir}/len_#{length}`
 end
